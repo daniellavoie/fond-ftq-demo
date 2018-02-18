a0e3659b87afbdc64831134a9a832dde52c3efdf
Daniel Lavoie <dlavoie@live.ca>
2018-02-18 12:22:01 -0500

    first commit

---
+++ ci/tasks/blue-green-deploy/blue-green-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Deploying to production"
+
+cf api $CF_API --skip-ssl-validation
+
+cf auth $CF_USER $CF_PASSWORD
+
+cf target -o $CF_ORG -s $CF_SPACE
+
+cf rename FortunesLegacyService FortunesLegacyService-old
+cf rename FortunesLegacyUI FortunesLegacyUI-old
+cf rename FortunesService FortunesService-old
+cf rename FortunesUI FortunesUI-old
+
+cf push -f src/ci/environments/manifest-$ENVIRONMENT.yml
+
+cf delete -f FortunesLegacyService-old
+cf delete -f FortunesLegacyUI-old
+cf delete -f FortunesService-old
+cf delete -f FortunesUI-old
---
+++ ci/tasks/smoke-tests/smoke-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "Running smoke tests"
